459be425856fa2ebdce129cd2075004a1e1c80df
annaselh <annas.elh@gmail.com>
2025-08-25 21:23:21 +0700

    1.2.0-rc.1

--- cicaresdkcall/build.gradle.kts
+++ cicaresdkcall/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 android {
     namespace = "cc.cicare.sdkcall"
-    version = "1.2.0-alpha.3"
+    version = "1.2.0-rc.1"
     compileSdk = 36
 
     defaultConfig {
@@ -90,7 +90,7 @@ afterEvaluate {
             create<MavenPublication>("release") {
                 groupId = "com.github.cicareteam"
                 artifactId = "cicare-sdkcall"
-                version = "1.2.0-alpha.3"
+                version = "1.2.0-rc.1"
                 from(components["release"])
             }
         }
